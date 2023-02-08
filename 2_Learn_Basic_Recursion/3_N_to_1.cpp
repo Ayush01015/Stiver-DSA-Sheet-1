042127d62cea37a1bde938d976eefbe50fae95ca
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-08 16:16:08 +0530

    N to 1 Recursion

---
+++ 2_Learn_Basic_Recursion/3_N_to_1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f1(int n){
+    if(n==0)
+        return;
+    else{
+        cout<<n<<" ";
+        f1(--n);
+    }
+}
+int main()
+{
+    cout<<"Enter a number\n";
+    int n;
+    cin>>n;
+    f1(n);
+    return 0;
+}
