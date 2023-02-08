fecbc81510718f443e256e13ccc2cdcaff7fb5d7
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-08 16:03:59 +0530

    Recursion Example

---
+++ 2_Learn_Basic_Recursion/0_understanding_recursion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f1(int n){
+    if(n==0)
+    return;
+    else{
+        cout<<n<<" ";
+        // n=n-1;
+        f1(--n);
+    }
+
+}
+int main()
+{   
+    cout<<"Enter a number\n";
+    int n;
+    cin>>n;
+    f1(n);    
+
+    return 0;
+}
