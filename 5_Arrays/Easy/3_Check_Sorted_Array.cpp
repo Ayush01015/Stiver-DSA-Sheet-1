784dedcc287f36746876f9239982358582e96c4c
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-16 11:50:57 +0530

    Check Array is sorted program

--- 5_Arrays/Easy/3_Check_Sorted_Array.cpp
+++ 5_Arrays/Easy/3_Check_Sorted_Array.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main()
-// {
-//     cout << "Enter value of N\n";
-//     int n;
-//     cin >> n;
-//     int a[n];
-//     int t = 0;
-//     cout << "Enter " << n << " values\n";
-//     for (int i = 0; i < n; i++)
-//         cin >> a[i];
-//     cout << "Your " << n << " values are: \n";
-//     for (int i = 0; i < n; i++)
-//         cout << a[i] << " ";
-//     for (int i = 0; i < n - 1; i++)
-//     {
-//         if (!(a[i] < a[i + 1]))
-//         {
-//             t++;
-//         }
-//     }
-//     if (t == 0)
-//         cout << "\nArray is sorted";
-//     else
-//         cout << "\nArray is not sorted";
-//     return 0;
-// }
+int main()
+{
+    cout << "Enter value of N\n";
+    int n;
+    cin >> n;
+    int a[n];
+    int t = 0;
+    cout << "Enter " << n << " values\n";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "Your " << n << " values are: \n";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (!(a[i] < a[i + 1]))
+        {
+            t++;
+        }
+    }
+    if (t == 0)
+        cout << "\nArray is sorted";
+    else
+        cout << "\nArray is not sorted";
+    return 0;
+}
