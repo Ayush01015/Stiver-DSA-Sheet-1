9bbf787d5a90ca9bf993c9913f3ab13ae07849cf
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-18 12:25:57 +0530

    Union of Arrays

--- 5_Arrays/Easy/4_Remove_Duplicates_in_Array.cpp
+++ 5_Arrays/Easy/4_Remove_Duplicates_in_Array.cpp
@@ -14,19 +14,16 @@
 //     cout << "Your " << n << " values are: \n";
 //     for (int i = 0; i < n; i++)
 //         cout << a[i] << " ";
-    
-//     for (int i = 0; i < n; i++){
-//         if(a[i]==a[i+1]){
-//             for(int j=i;j<n-1;j++){
-//                 a[j]=a[j+1];
-//             }
-//             cout<<endl; 
-//             for (int i = 0; i < n; i++)
-//              cout << a[i] << " ";
-//             cout<<endl; 
-//         }
+//     vector<int> v;
+//     v.push_back(a[0]);
+//     for(int i=1;i<n;i++){
+//         if(a[i]!=a[i-1])
+//             v.push_back(a[i]);
+//     }
+//     cout<<endl;
+//     for(auto it: v){
+//         cout<<it<<" ";
 //     }
     
 //     return 0;
 // }
-//not complete!!!!
--- 5_Arrays/Easy/9_Union_OF_arrays.cpp
+++ 5_Arrays/Easy/9_Union_OF_arrays.cpp
@@ -3,12 +3,34 @@ using namespace std;
 
 int main()
 {
-    int a1[] = {1,2,2,2,2,3,4,5,6,7,8,9,10};
-    int a2[] = {2,3,4,4,5,11,12};
+    int a1[] = {1,2,3,4,5};
+    int a2[] = {2,3,4,4,5};
+    int m = 5 , n = 5;
     vector <int> temp;
     vector <int> hash;
-    
-
-      
+    for(int i=0;i<m;i++){
+        temp.push_back(a1[i]);
+    }
+    for(int i=0;i<n;i++){
+        temp.push_back(a2[i]);
+    }
+    for(auto it: temp){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    sort(temp.begin(),temp.end());
+    for(auto it: temp){
+        cout<<it<<" ";
+    }
+    vector<int>v;
+    v.push_back(temp[0]);
+    for(int i=1;i<temp.size();i++){
+        if(temp[i]!=temp[i-1])
+            v.push_back(temp[i]);
+    }
+    cout<<endl;
+    for(auto it: v){
+        cout<<it<<" ";
+    }
     return 0;
 }
