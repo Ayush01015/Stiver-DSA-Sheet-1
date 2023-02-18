d2dfb8ec1c95d4e40e341786c95a304e2cf09f42
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-18 12:26:29 +0530

    Remove Duplicates In Array

--- 5_Arrays/Easy/4_Remove_Duplicates_in_Array.cpp
+++ 5_Arrays/Easy/4_Remove_Duplicates_in_Array.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main()
-// {
-//     cout << "Enter value of N\n";
-//     int n;
-//     cin >> n;
-//     int a[n]; //arr[1,1,2,2,2,3,3]
-//     int t = 0;
-//     cout << "Enter " << n << " values\n";
-//     for (int i = 0; i < n; i++)
-//         cin >> a[i];
-//     cout << "Your " << n << " values are: \n";
-//     for (int i = 0; i < n; i++)
-//         cout << a[i] << " ";
-//     vector<int> v;
-//     v.push_back(a[0]);
-//     for(int i=1;i<n;i++){
-//         if(a[i]!=a[i-1])
-//             v.push_back(a[i]);
-//     }
-//     cout<<endl;
-//     for(auto it: v){
-//         cout<<it<<" ";
-//     }
+int main()
+{
+    cout << "Enter value of N\n";
+    int n;
+    cin >> n;
+    int a[n]; //arr[1,1,2,2,2,3,3]
+    int t = 0;
+    cout << "Enter " << n << " values\n";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "Your " << n << " values are: \n";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    vector<int> v;
+    v.push_back(a[0]);
+    for(int i=1;i<n;i++){
+        if(a[i]!=a[i-1])
+            v.push_back(a[i]);
+    }
+    cout<<endl;
+    for(auto it: v){
+        cout<<it<<" ";
+    }
     
-//     return 0;
-// }
+    return 0;
+}
