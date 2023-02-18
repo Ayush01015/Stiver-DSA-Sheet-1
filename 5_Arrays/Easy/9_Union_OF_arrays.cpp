5c73d8ffdd20a443fe2bec08e2aed924c8014487
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-18 11:53:55 +0530

    Saving previous codes

---
+++ 5_Arrays/Easy/9_Union_OF_arrays.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a1[] = {1,2,2,2,2,3,4,5,6,7,8,9,10};
+    int a2[] = {2,3,4,4,5,11,12};
+    vector <int> temp;
+    vector <int> hash;
+    
+
+      
+    return 0;
+}
