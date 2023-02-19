71b675ceef7ecdeb0e7ff2850b89ce3e5660ba0d
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-20 02:51:04 +0530

    Kadane's Algorithm [Maximum Sub-Array sum in an Array]

---
+++ 5_Arrays/Medium/4_Kadane_algo.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    cout << "Enter n: ";
+    int n;
+    cin >> n;
+    vector<int> v;
+    cout << "Enter " << n << " values" << endl;
+    int l;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> l;
+        v.push_back(l);
+    }
+    cout << "Elements are: ";
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+    int sum = 0, temp;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            temp=0;
+            for(int k=i;k<=j;k++){
+                temp = temp + v[k];
+            }
+            if(sum<temp)
+                sum=temp;
+        }
+    }
+    cout<<"Sum: "<<sum<<endl;
+
+    return 0;
+}
