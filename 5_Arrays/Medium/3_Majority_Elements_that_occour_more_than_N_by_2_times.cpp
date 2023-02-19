7fccada509387c5c0d87e7fa07e169c9a5969221
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-19 14:23:41 +0530

    failing test case 1 in majority

--- 5_Arrays/Medium/3_Majority_Elements_that_occour_more_than_N_by_2_times.cpp
+++ 5_Arrays/Medium/3_Majority_Elements_that_occour_more_than_N_by_2_times.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
 int main()
 {
     cout << "Enter n: ";
     int n;
     cin >> n;
     vector<int> v;
+    int hash[100000];
     cout << "Enter " << n << " values" << endl;
     int l;
     for (int i = 0; i < n; i++)
     {
         cin >> l;
         v.push_back(l);
+        hash[v[i]]+=1;
+
     }
     cout << "Elements are: ";
     for (auto it : v)
     {
         cout << it << " ";
+    }   
+    cout << endl;
+    int N = n/2;
+    for(int i=0;i<1;i++){
+        cout<<hash[i]<<" ";
     }
     cout << endl;
-    vector <int> temp;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<n;j++){
-            if(v[j]==i)
-                temp.push_back(i);
-        }
+    for(int i=0;i<n;i++){
+        if(hash[i]>N)
+            cout<<"Result: "<<i<<endl;
     }
-    for (auto it : temp)
-    {
-        cout << it << " ";
-    }
-
     return 0;
 }
+// Input Format:  N = 10, nums[] = {4,4,2,4,3,4,4,3,2,4}
+// Result: 4
