6352188470bc5a7b5df24d848d99ea2adfe4143a
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-05 02:13:48 +0530

    By Dutch National Flag Algorithm O(N)

--- 5_Arrays/Medium/2_sort_an_Array_of_0's_1's_2's.cpp
+++ 5_Arrays/Medium/2_sort_an_Array_of_0's_1's_2's.cpp
@@ -21,17 +21,58 @@ int main()
         cout << it << " ";
     }
     cout << endl;
-    vector <int> temp;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<n;j++){
-            if(v[j]==i)
-                temp.push_back(i);
+
+    // vector <int> temp;
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(v[j]==i)
+    //             temp.push_back(i);
+    //     }
+    // }
+
+    // for (auto it : temp)
+    // {
+    //     cout << it << " ";
+    // }
+
+    //By Algorithm(DNFA)
+
+    int low=0,mid=0,high=v.size()-1; 
+    while(mid<=high){  //O(n)
+        switch(v[mid]){
+            case 0: 
+                swap(v[low++],v[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(v[mid],v[high--]);
+                break;
         }
     }
-    for (auto it : temp)
+    cout << "Sorted Elements are: ";
+    for (auto it : v)
     {
         cout << it << " ";
     }
+    cout << endl;
 
     return 0;
 }
+
+/*
+Dutch National Flag Algorithm O(N)
+
+{0...low-1}   --> 0
+{high+1...n}  --> 2
+{low...mid-1} --> 1
+
+case 0:
+    swap(v[low++],vmid++])
+case 1:
+    mid++;
+case 2:
+    swap(v[mid],v[high--])
+
+*/
