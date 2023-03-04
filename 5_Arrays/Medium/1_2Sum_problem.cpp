d2c4082c39cd1e137a1190c8ef10045ec441987d
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-04 22:13:44 +0530

    Correct Approach [2 sum Problem O(n^2) ]

--- 5_Arrays/Medium/1_2Sum_problem.cpp
+++ 5_Arrays/Medium/1_2Sum_problem.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-void twoSum(vector<int> v, int target)
-{
-    int a=-1, b=-1;
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i] + v[(i + 1) % v.size()] == target)
-        {
-            a = i;
-            b = (i + 1) % v.size();
-            break;
-        }
-    }
-    cout << "[" << a << "," << b << "]" << endl;
-}
 
+vector<int> twoSum2(vector<int>& nums, int target) {
+    vector<int> res;
+    for (int i = 0; i < nums.size(); ++i) {
+   	 for (int j = i + 1; j < nums.size(); ++j) {
+   		 if (nums[i] + nums[j] == target) {
+   			 res.emplace_back(i);
+   			 res.emplace_back(j);
+   			 break;
+   		 }
+   	 }
+   	 if (res.size() == 2)
+   		 break;
+    }
+    return res;
+}
 int main()
 {
     cout << "Enter n: ";
@@ -34,7 +35,13 @@ int main()
         cout << it << " ";
     }
     cout << endl;
-    twoSum(v,6);
+    // twoSum(v,10);
+    vector <int> c;
+    c=twoSum2(v,10);
+    for(auto it:c){
+        cout<<it<<" ";
+    }
+
 
     return 0;
 }
